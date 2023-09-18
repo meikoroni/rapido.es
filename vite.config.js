d9df4b99f61df046fe75f051ee4a3bcd82513601
veronica lopez <meikoroni@hotmail.com>
2023-09-18 21:08:26 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import path, { resolve } from 'path'
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve :{
+        alias :{
+    '~bootstrap': path.resolve (__dirname, 'node_modules/bootstrap'),
+        }
+    },
+
 });
