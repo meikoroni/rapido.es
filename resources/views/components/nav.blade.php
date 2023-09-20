f47261e5fc9fe82f38260cf66c4043c252966aa7
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-09-20 16:14:49 +0200

    final navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,7 +4,7 @@
     <form class="d-flex" role="search">
         <input class="form-control me-1 search-box" type="search" placeholder="Buscar en todas las categorías..." aria-label="Search">
     </form>
-    <nav class="nav-bar">
+    <nav>
     <div class="nav-list">
             <button class="boton-blanco" href="#">Regístrate o Inicia sesión</button>
             <button class="boton-verde">Subir artículo</button>
@@ -12,4 +12,13 @@
     </div>
     </nav>
 </header>
+<div class="nav-bottom">
+    <ul class="nav-bottom-list">
+        <li><img class="img-botton-nav" src="{{ asset('tienda.svg') }}" alt="inicio"> Inicio</li>
+        <li><img class="img-botton-nav" src="{{ asset('heart.svg') }}" alt="favoritos"> Favorito</li>
+        <li><img class="img-botton-nav" src="{{ asset('mas.svg') }}" alt="subelo"> Subelo</li>
+        <li><img class="img-botton-nav" src="{{ asset('email.svg') }}" alt="buzon"> Buzon</li>
+        <li><img class="img-botton-nav" src="{{ asset('cara.svg') }}" alt="tu-perfil">Tu perfil</li>
+    </ul>
+</div>
 
